a6cc2e10fee773841807d839390959655722c875
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-09-10 20:30:19 +0800

    Add styles for FaTimes

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,9 +4,8 @@ import {FaTimes} from 'react-icons/fa';
 const Task = ({task}) => {
   return (
     <div className="task">
-        <h3>{task.taskText} <FaTimes /></h3>
+        <h3>{task.taskText} <FaTimes style={{color:'red', cursor:'pointer'}}/></h3>
         <h3>{task.day}</h3>
-
     </div>
   )
 }

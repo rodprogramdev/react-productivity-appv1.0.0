c4705db466046c27c931acb19c7835805423c664
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-09-14 16:14:33 +0800

    Add react onClick test test for onClick react events Test for adding prop onclick react events

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,13 @@
 import PropTypes from 'prop-types';
 
-const Button = ({bgColor,dynamicText}) => {
-    return <button style={{bgColor}} className='button'>{dynamicText}</button>
+const Button = ({bgColor,dynamicText,onClickProp}) => {
+  
+    // const onClick = (eventObject) => {
+    //     alert('This is a a test for react  onclick attribute test')
+    //     console.log(eventObject);
+    // }
+    
+    return <button style={{bgColor}} className='button' onClick={onClickProp}>{dynamicText}</button>
 
     
 }
@@ -9,5 +15,12 @@ const Button = ({bgColor,dynamicText}) => {
 Button.defaultProps = {
     bgColor: 'green'
 }
+
+Button.propTypes = {
+    dynamicText: PropTypes.string,
+    color: PropTypes.string
+}
+
+
 
 export default Button;
--- src/components/ShowHideSection.js
+++ src/components/ShowHideSection.js
@@ -1,9 +1,20 @@
 import Button from './Button'
 
 const ShowHideSection = () => {
+  const onClick = (eventObject) => {
+    alert('This is a a test for react props onclick')
+    console.log(eventObject);
+}
+
   return (
-    <Button dynamicText='Show' />   
+    <Button dynamicText='Show' onClickProp={onClick} />   
   )
+   
+
+    
+   
+    
+  
 }
 
 export default ShowHideSection

c7b1461ee8e82dd2f9ce8aa2a57d051e58156bde
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-09-16 23:14:54 +0800

    issue with bottomNavigation

--- src/App.js
+++ src/App.js
@@ -17,18 +17,35 @@ function App() {
       idRef: 1,
       taskText: 'Study react',
       day: 'September 10, 2023',
+      motivation:'“All our dreams can come true, if we have the courage to pursue them.” —Walt Disney',
       reminder: false
     },
     {
       idRef: 2,
       taskText: 'Study material ui',
       day: 'September 11, 2023',
+      motivation:'“If people are doubting how far you can go, go so far that you can’t hear them anymore.” —Michele Ruiz',
       reminder: true
     },
     {
       idRef: 3,
       taskText: 'Build a project with react',
       day: 'September 12, 2023',
+      motivation: '“The same boiling water that softens the potato hardens the egg. It’s what you’re made of. Not the circumstances.” —Unknown',
+      reminder: true
+    },
+    {
+      idRef: 4,
+      taskText: 'Build a project with react',
+      day: 'September 12, 2023',
+      motivation: '"Learning is a matter of finding your purpose, find your purpose, and you will be the wisest man who ever lived."- Rodrigo Bonzerr S. Lopez',
+      reminder: true
+    },
+    {
+      idRef: 5,
+      taskText: 'Build a project with react',
+      day: 'September 12, 2023',
+      motivation: '"Those people who knows, know, those people who wants to learn listens, sooner or later those people who knows, knows more than those people who says that they know."- Smart Leadership By: Jonathan Yudolowitz, Richard Koch, Robin Field',
       reminder: true
     }
   ]);
